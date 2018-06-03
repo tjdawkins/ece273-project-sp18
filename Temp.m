2ce3ed6348da263d0c9c5a3aeb38607872344b0f
Thomas J D Dawkins <tjdawkins@redtreeit.com>
2018-06-03 12:11:18 -0700

    Update directory for data

--- Temp.m
+++ Temp.m
@@ -1,6 +1,6 @@
 % Change the filenames if you've saved the files under different names
 % On some platforms, the files might be saved as 
 % train-images.idx3-ubyte / train-labels.idx1-ubyte
-images = loadMNISTImages('t10k-images.idx3-ubyte');
-% labels = loadMNISTLabels('train-labels-idx1-ubyte');
+tr_images = loadMNISTImages('Data/t10k-images.idx3-ubyte');
+tr_labels = loadMNISTLabels('Data/train-labels-idx1-ubyte');
  
