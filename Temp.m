eae346ae011eba19ea93a5bf16ef0fd446b23e90
Thomas J D Dawkins <tjdawkins@redtreeit.com>
2018-06-03 11:59:09 -0700

    Remove display function

--- Temp.m
+++ Temp.m
@@ -4,6 +4,3 @@
 images = loadMNISTImages('t10k-images.idx3-ubyte');
 % labels = loadMNISTLabels('train-labels-idx1-ubyte');
  
-% We are using display_network from the autoencoder code
-display_network(images(:,1:100)); % Show the first 100 images
-% disp(labels(1:10));
